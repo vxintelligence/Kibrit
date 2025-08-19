dc3fc44d31ae23f16cce5e346c75960b63e88730
TOUHAMI KASBAOUI <31594437+sqrtZeroKnowledge@users.noreply.github.com>
2025-08-19 16:08:20 +0100

    add LuaJIT lib and headers

--- Kibrit/premake5.lua
+++ Kibrit/premake5.lua
@@ -2,45 +2,49 @@ project "Kibrit"
    kind "ConsoleApp"
    language "C++"
    cppdialect "C++17"
-   targetdir "bin/%{cfg.buildcfg}"
    staticruntime "off"
-
+   
+   -- Use consistent targetdir (remove the duplicate)
+   targetdir ("../bin/" .. outputdir .. "/%{prj.name}")
+   objdir ("../bin-int/" .. outputdir .. "/%{prj.name}")
+   
    files { "src/**.h", "src/**.cpp" }
-
+   
    includedirs
    {
       "../vendor/imgui",
       "../vendor/glfw/include",
-
       "../uil/src",
-
       "%{IncludeDir.VulkanSDK}",
       "%{IncludeDir.glm}",
+      "%{IncludeDir.LuaJIT}",
    }
-
-    links
-    {
-        "uil"
-    }
-
-   targetdir ("../bin/" .. outputdir .. "/%{prj.name}")
-   objdir ("../bin-int/" .. outputdir .. "/%{prj.name}")
-
+   
+   libdirs
+   {
+      "%{LibraryDir.LuaJIT}"
+   }
+   
+   links
+   {
+      "uil"
+   }
+   
    filter "system:windows"
       systemversion "latest"
       defines { "WL_PLATFORM_WINDOWS" }
-
+      
    filter "configurations:Debug"
       defines { "WL_DEBUG" }
       runtime "Debug"
       symbols "On"
-
+      
    filter "configurations:Release"
       defines { "WL_RELEASE" }
       runtime "Release"
       optimize "On"
       symbols "On"
-
+      
    filter "configurations:Dist"
       kind "WindowedApp"
       defines { "WL_DIST" }
