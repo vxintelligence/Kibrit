d20da4a679c8b585074c65b27f5f2e0bc0488995
TOUHAMI KASBAOUI <31594437+sqrtZeroKnowledge@users.noreply.github.com>
2025-08-19 16:07:15 +0100

    Update uilex.lua

--- uilex.lua
+++ uilex.lua
@@ -5,9 +5,11 @@ VULKAN_SDK = os.getenv("VULKAN_SDK")
 IncludeDir = {}
 IncludeDir["VulkanSDK"] = "%{VULKAN_SDK}/Include"
 IncludeDir["glm"] = "../vendor/glm"
+IncludeDir["LuaJIT"] = "../vendor/LuaJIT/src"
 
 LibraryDir = {}
 LibraryDir["VulkanSDK"] = "%{VULKAN_SDK}/Lib"
+LibraryDir["LuaJIT"] = "../vendor/LuaJIT/src"
 
 Library = {}
 Library["Vulkan"] = "%{LibraryDir.VulkanSDK}/vulkan-1.lib"
